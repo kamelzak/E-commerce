3b91293aa603365dac3c76c5a51945cb348beeb3
KamelZak <gk_zaknoune@esi.dz>
2021-12-20 18:42:36 +0100

    Adding categories controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,12 +4,57 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
-    public function products()
+    public function index()
     {
-        return $this->belongToMany(Product::class);
+        $categories = Category::all();
+        return response($categories, 200);
     }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'slug' => 'required'
+        ]);
+        $category = Category::create($data);
+        return response($category, 200);
+    }
+
+    public function show($id)
+    {
+        $category = Category::findOrFail($id);
+        return response($category, 200);
+    }
+
+    public function edit($id)
+    {
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'slug' => 'required'
+        ]);
+        $category = Category::where('id', $id)->update($data, $id);
+        return response($category, 200);
+    }
+
+    public function destroy($id)
+    {
+        $category=Category::find($id);
+        $category->delete();
+        return response('category deleted', 200);
+    }
+
 }
